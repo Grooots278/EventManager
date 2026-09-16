a62d21cf2b805acff410d777d78c997e4fb67a85
Dmitry <dolphin1309@mail.com>
2026-09-16 22:19:33 +0700

    feat(backend):  AppDbContext

---
+++ backend/src/EventManager.Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using EventManager.Application.Common.Interfaces;
+using EventManager.Domain.Authentication;
+using EventManager.Domain.Cities;
+using EventManager.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventManager.Infrastructure.Persistence;
+
+public sealed class AppDbContext : 
+    DbContext, IApplicationDbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+
+    public DbSet<User> Users => Set<User>();
+    public DbSet<City> Cities => Set<City>();
+    public DbSet<RefreshSession> RefreshSessions => Set<RefreshSession>();
+    public DbSet<UserProfile> UserProfiles => Set<UserProfile>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.
+            ApplyConfigurationsFromAssembly(typeof(AppDbContext)
+            .Assembly);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
