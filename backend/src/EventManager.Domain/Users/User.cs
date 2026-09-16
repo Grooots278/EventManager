80dbb3526ac5a2df22684548d8d5cc5fcd3abb81
Dmitry <dolphin1309@mail.com>
2026-09-16 19:37:28 +0700

    feat(backend): domain users modals(User, UserProfile)

---
+++ backend/src/EventManager.Domain/Users/User.cs
@@ -0,0 +1,57 @@
+﻿using EventManager.Domain.Abstractions;
+using EventManager.Domain.Common;
+using EventManager.Domain.Users.ValueObjects;
+
+namespace EventManager.Domain.Users;
+
+public sealed class User : AggregateRoot, IAuditable
+{
+    public Login Login { get; private set; } = null!;
+    public PasswordHash PasswordHash { get; private set; } = null!;
+    public UserProfile Profile { get; private set; } = null!;
+    public DateTime CreatedAtUtc { get; set; }
+    public DateTime? UpdatedAtUtc { get; set; }
+
+    private User() { }
+
+    private User(
+        Login login,
+        PasswordHash password,
+        UserProfile profile)
+    {
+        Login = login;
+        PasswordHash = password;
+        Profile = profile;
+    }
+
+    public static User Create(
+        Login login,
+        PasswordHash passwordHash,
+        FirstName firstName,
+        LastName lastName,
+        Email email,
+        DateOnly birthDate,
+        Guid? cityId)
+    {
+        var user = new User(
+            login,
+            passwordHash,
+            null!);
+
+        user.Profile = new UserProfile(
+            user.Id,
+            firstName,
+            lastName,
+            email,
+            birthDate,
+            cityId);
+
+        return user;
+    }
+
+    public void ChangePassword(PasswordHash passwordHash)
+    {
+        if (PasswordHash == passwordHash) return;
+        PasswordHash = passwordHash;
+    }
+}
---
+++ backend/src/EventManager.Domain/Users/UserProfile.cs
@@ -0,0 +1,82 @@
+﻿using EventManager.Domain.Abstractions;
+using EventManager.Domain.Enums;
+using EventManager.Domain.Users.ValueObjects;
+
+namespace EventManager.Domain.Users
+{
+    public sealed class UserProfile : IAuditable
+    {
+        public Guid UserId { get; private set; }
+        public FirstName FirstName { get; private set; } = null!;
+        public LastName LastName { get; private set; } = null!;
+        public Email Email { get; private set; } = null!;
+        public DateOnly BirthDate { get; private set; }
+        public Guid? CityId { get; private set; }
+        public UserRole Role { get; private set; }
+        public bool IsActive { get; private set; }
+        public DateTime CreatedAtUtc { get; set; }
+        public DateTime? UpdatedAtUtc { get; set; }
+
+        private UserProfile() { }
+
+        internal UserProfile(
+            Guid userId,
+            FirstName firstName,
+            LastName lastName,
+            Email email,
+            DateOnly birthDate,
+            Guid? cityId)
+        {
+            UserId = userId;
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            BirthDate = birthDate;
+            CityId = cityId;
+            Role = UserRole.User;
+            IsActive = true;
+        }
+
+        public void ChangeName(
+            FirstName firstName,
+            LastName lastName)
+        {
+            if (firstName == FirstName && LastName == LastName) return;
+
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public void ChangeEmail(
+            Email email)
+        {
+            if (Email == email) return;
+            Email = email;
+        }
+
+        public void ChangeCity(Guid? cityId)
+        {
+            if (cityId == CityId) return;
+            CityId = cityId;
+        }
+
+        public void Deactivate()
+        {
+            if (!IsActive) return;
+            IsActive = false;
+        }
+
+        public void Activate()
+        {
+            if (IsActive) return;
+            IsActive = true;
+        }
+
+        public void ChangeRole(
+            UserRole role)
+        {
+            if (Role == role) return;
+            Role = role;
+        }
+    }
+}
