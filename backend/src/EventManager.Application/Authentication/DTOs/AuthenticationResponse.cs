fb27b8fe795c16f22d53d85e9a2ba4ea02733e7c
Dmitry <dolphin1309@mail.com>
2026-09-16 22:51:47 +0700

    feat(backend): Authentication response dto

---
+++ backend/src/EventManager.Application/Authentication/DTOs/AuthenticationResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace EventManager.Application.Authentication.DTOs;
+
+public sealed record AuthenticationResponse(
+    string AccessToken,
+    string RefreshToken,
+    DateTime AccessTokenExpiresAtUts
+);
