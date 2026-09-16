1a4f5292d0fb5f156e060181af06ed1210f87b95
Dmitry <dolphin1309@mail.com>
2026-09-16 22:44:40 +0700

    feat(backend):  Password hasher functional (Application, Infrastructure)

---
+++ backend/src/EventManager.Infrastructure/Authentication/PasswordHasher.cs
@@ -0,0 +1,37 @@
+﻿using EventManager.Application.Common.Interfaces;
+using Microsoft.AspNetCore.Identity;
+
+namespace EventManager.Infrastructure.Authentication;
+
+public sealed class PasswordHasher
+    : IPasswordHasher
+{
+    private readonly Microsoft.AspNetCore.Identity.PasswordHasher<object>
+        _hasher = new();
+
+    public string Hash(string password)
+    {
+        if (string.IsNullOrWhiteSpace(password))
+            throw new ArgumentException(
+                "Password cannot be empty.");
+
+        return _hasher.HashPassword(
+            new object(),
+            password);
+    }
+
+    public bool Verify(
+        string password,
+        string passwordHash)
+    {
+        var result = _hasher.VerifyHashedPassword(
+            new object(),
+            passwordHash,
+            password);
+
+        return result ==
+            PasswordVerificationResult.Success ||
+            result ==
+            PasswordVerificationResult.SuccessRehashNeeded;
+    }
+}
