9594bc61faff9cd62cd08883cfea78d4b634f7fd
Dmitry <dolphin1309@mail.com>
2026-09-17 00:53:42 +0700

    feat(backend): exceptions(conflict, notfound, unauthorized)

---
+++ backend/src/EventManager.Application/Common/Exceptions/ConflictException.cs
@@ -0,0 +1,7 @@
+﻿namespace EventManager.Application.Common.Exceptions;
+
+public sealed class ConflictException : Exception
+{
+    public ConflictException(string message)
+        : base(message) { }
+}
---
+++ backend/src/EventManager.Application/Common/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,8 @@
+﻿namespace EventManager.Application.Common.Exceptions;
+
+public sealed class UnauthorizedException
+    : Exception
+{
+    public UnauthorizedException
+        (string message) : base (message) { }
+}
