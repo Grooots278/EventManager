2bdd70046e6846499d71f1706ccc5ab5af1e9bc2
Dmitry <dolphin1309@mail.com>
2026-09-16 20:43:11 +0700

    feat(backend): dbcontext interface

---
+++ backend/src/EventManager.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using EventManager.Domain.Authentication;
+using EventManager.Domain.Cities;
+using EventManager.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventManager.Application.Common.Interfaces;
+
+public interface IApplicationDbContext
+{
+    DbSet<User> Users { get; }
+
+    DbSet<UserProfile> UserProfiles { get; }
+
+    DbSet<City> Cities { get; }
+
+    DbSet<RefreshSession> RefreshSessions { get; }
+
+    Task<int> SaveChangesAsync(
+        CancellationToken cancellationToken);
+}
