fd1709ad9cfdb28667d391432d3584b46d5bef1c
Dmitry <dolphin1309@mail.com>
2026-09-16 20:42:34 +0700

    feat(backend): new modal(City)

---
+++ backend/src/EventManager.Domain/Cities/City.cs
@@ -0,0 +1,39 @@
+﻿using EventManager.Domain.Common;
+
+namespace EventManager.Domain.Cities;
+
+public sealed class City : Entity
+{
+    public string Name { get; private set; } = null!;
+
+    private City() { }
+
+    private City(string name) => Name = name;
+
+    public static City Create(string name)
+    {
+        ValidateName(ref name);
+
+        return new City(name);
+    }
+
+    public void Rename(string name)
+    {
+        if (Name == name) return;
+        ValidateName(ref name);
+        Name = name;
+    }
+
+    private static void ValidateName(ref string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new DomainException(
+                "City name is required.");
+
+        name = name.Trim();
+
+        if (name.Length > 100)
+            throw new DomainException(
+                "City name is too long.");
+    }
+}
