62f1bd2cad03779e1114eb24b4424dcfbd350cdf
Dmitry <dolphin1309@mail.com>
2026-09-16 15:55:12 +0700

    chore: initialize project architecture

---
+++ backend/src/EventManager.Worker/Program.cs
@@ -0,0 +1,7 @@
+using EventManager.Worker;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
---
+++ backend/tests/EventManager.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace EventManager.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
