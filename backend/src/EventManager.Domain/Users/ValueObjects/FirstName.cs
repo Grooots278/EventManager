f2168501f7ac85a6b291716eda7e10b21f93dd13
Dmitry <dolphin1309@mail.com>
2026-09-16 18:55:29 +0700

    feat(backend): ValueObjects (FirstName, LastName)

---
+++ backend/src/EventManager.Domain/Users/ValueObjects/FirstName.cs
@@ -0,0 +1,29 @@
+﻿using EventManager.Domain.Common;
+
+namespace EventManager.Domain.Users.ValueObjects;
+
+public sealed class FirstName
+{
+    public string Value { get; }
+
+    public FirstName(string value)
+        => Value = value;
+
+    public static FirstName Create(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new DomainException(
+                "First name is required.");
+
+        value = value.Trim();
+
+        if (value.Length > 100)
+            throw new DomainException(
+                "First name is too long.");
+
+        return new FirstName(value);
+    }
+
+    public override string ToString()
+        => Value;
+}
---
+++ backend/src/EventManager.Domain/Users/ValueObjects/LastName.cs
@@ -0,0 +1,29 @@
+﻿using EventManager.Domain.Common;
+
+namespace EventManager.Domain.Users.ValueObjects;
+
+public sealed class LastName
+{
+    public string Value { get; }
+
+    public LastName(string value) =>
+        Value = value;
+
+    public static LastName Create(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new DomainException(
+                "Last name is required.");
+
+        value = value.Trim();
+
+        if (value.Length > 100)
+            throw new DomainException(
+                "Last name is too long");
+
+        return new LastName(value);
+    }
+
+    public override string ToString()
+        => Value;
+}
