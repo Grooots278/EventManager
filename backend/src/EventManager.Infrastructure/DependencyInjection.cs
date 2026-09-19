9e020bd3780dd0486cf40cc98baa2e88d223d437
Dmitry <dolphin1309@mail.com>
2026-09-19 11:04:28 +0700

    feat(backend): Dependency injection (Infrastructure)

---
+++ backend/src/EventManager.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,43 @@
+using EventManager.Application.Common.Interfaces;
+using EventManager.Domain.Users.ValueObjects;
+using EventManager.Infrastructure.Authentication;
+using EventManager.Infrastructure.Options;
+using EventManager.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventManager.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
+    {
+        services.AddDbContext<AppDbContext>(
+            options =>
+            {
+                options.UseNpgsql(
+                    configuration.GetConnectionString(
+                        "Database"
+                    )
+                );
+            }
+        );
+
+        services.AddScoped<IApplicationDbContext>(
+            provider => provider.GetRequiredService<AppDbContext>()
+        );
+
+        services.AddScoped<IJWTTokenService, JWTTokenService>();
+        services.AddScoped<IPasswordHasher, PasswordHasher>();
+
+        services.Configure<JwtOptions>(
+            configuration.GetSection(JwtOptions.SectionName)
+        );
+
+        return services;
+    }
+}
