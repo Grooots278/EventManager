c9477bee3865a63fb80abe083c95c1af17b40acc
Dmitry <dolphin1309@mail.com>
2026-09-20 17:31:15 +0700

    feat(backend): UserController(UpdateProfile, ChangePassword)

--- backend/src/EventManager.API/Controllers/UsersController.cs
+++ backend/src/EventManager.API/Controllers/UsersController.cs
@@ -1,3 +1,6 @@
+using EventManager.API.Contracts.Users;
+using EventManager.Application.Users.Commands.ChangePassword;
+using EventManager.Application.Users.Commands.UpdateProfile;
 using EventManager.Application.Users.Queries.GetCurrentUser;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -25,5 +28,40 @@ public sealed class UsersController : ControllerBase
                 cancellationToken);
 
         return Ok(result);
+    }
+
+    [HttpPut("me/profile")]
+    public async Task<IActionResult> UpdateProfile(
+        UpdateProfileRequest request,
+        CancellationToken cancellationToken)
+    {
+        var command = 
+            new UpdateProfileCommand(
+                request.FirstName,
+                request.LastName,
+                request.BirthDate,
+                request.CityId);
+
+        await _sender.Send(
+            command,
+            cancellationToken);
+
+        return NoContent();
+    }
+
+    [HttpPut("me/password")]
+    public async Task<IActionResult> ChangePassword(
+        ChangePasswordRequest request, 
+        CancellationToken cancellationToken)
+    {
+        var command = 
+            new ChangePasswordCommand(
+                request.CurrentPassword,
+                request.NewPassword);
+
+        await _sender.Send(command, 
+            cancellationToken);
+
+        return NoContent();
     }
 }
