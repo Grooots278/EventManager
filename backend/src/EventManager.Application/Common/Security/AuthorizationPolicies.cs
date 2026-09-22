a697d2cfb126ba6590bc386ba2519afe6b8d1f1e
Dmitry <dolphin1309@mail.com>
2026-09-22 13:56:30 +0700

    feat(backend): Authorization policies

---
+++ backend/src/EventManager.Application/Common/Security/AuthorizationPolicies.cs
@@ -0,0 +1,10 @@
+namespace EventManager.Application.Common.Security;
+
+public sealed class AuthorizationPolicies
+{
+    public const string AdminOnly = 
+        "AdminOnly";
+
+    public const string Authenticated = 
+        "Authenticated";
+}
